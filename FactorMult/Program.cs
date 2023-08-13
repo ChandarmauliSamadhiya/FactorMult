ee0bf68bdb2dfb3d17b5e0a144ce887523c60491
acer <acer@ASUR>
2023-08-13 21:41:13 +0530

    Add project files.

---
+++ FactorMult/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorMult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int factor = 2;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] *= factor;
+            }
+
+            Console.WriteLine("Multiplied array:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
